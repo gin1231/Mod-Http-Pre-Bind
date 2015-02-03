538b40060879d5ecb218c3784ce9817eee4b3ff9
gin1231 <jineok12@gmail.com>
2015-02-03 15:08:19 +0900

    build files, cp added

--- build.sh
+++ build.sh
@@ -1,2 +1,7 @@
 #!/bin/sh
 erl -pa /usr/lib/ejabberd/ebin -pz ebin -make
+
+cp ebin/mod_http_pre_bind.beam /usr/lib/ejabberd/ebin
+cp ebin/mod_http_pre_bind.beam /lib/ejabberd/ebin
+cp ebin/ejabberd_http_pre_bind.beam /usr/lib/ejabberd/ebin
+cp ebin/ejabberd_http_pre_bind.beam /lib/ejabberd/ebin
